978c32842ee95ec323a963e28fae947d13f88047
seokhee123 <seokhee1203@naver.com>
2022-09-07 17:17:32 +0900

    Scale, Minimap update

--- Assets/Scripts/Scale.cs
+++ Assets/Scripts/Scale.cs
@@ -5,16 +5,38 @@ using UnityEngine;
 public class Scale : MonoBehaviour
 {
     public float totalMass = 0;
-    public Rigidbody rb;
+    //public List<Rigidbody> rb = new List<Rigidbody>();
     public List<GameObject> checkMass = new List<GameObject>();
 
+    void Update()
+    {
+    }
+
+/*    void addMass()
+    {
+        for(int i=0;i<checkMass.Count; i++)
+        {
+            totalMass += checkMass[i].GetComponent<Rigidbody>().mass;
+            
+        }
+        
+    }*/
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Scale")
         {
             checkMass.Add(collision.gameObject);
-            //rb = checkMass.GetComponent<Rigidbody>();
-            totalMass += rb.mass;
+            totalMass += collision.rigidbody.mass;
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if(collision.gameObject.tag == "Scale")
+        {
+            //checkMass.Find(x => x.name == collision.gameObject.name);
+            checkMass.Remove(collision.gameObject);
+            totalMass -= collision.rigidbody.mass;
         }
     }
 }
